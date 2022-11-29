09c92aef1e2664e84a90179ea14ba14258e92d26
Denis Petrovskii <denpetrovsky369@gmail.com>
2022-11-29 05:02:38 +0300

    100persenttest

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -15,4 +15,17 @@ public class MaxServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void shouldFindMaxIfB() {
+        MaxService servise = new MaxService();
+
+        int a = 3;
+        int b = 5;
+
+        int expected = b;
+        int actual = servise.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
