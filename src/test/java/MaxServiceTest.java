45cccba3832bc501f685487671578ebf40fedb76
Denis Petrovskii <denpetrovsky369@gmail.com>
2022-11-29 04:32:02 +0300

    secondcommit

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class MaxServiceTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MaxServiceTest {
+
+    @Test
+    public void shouldFindMax() {
+        MaxService servise = new MaxService();
+
+        int a = 5;
+        int b = 3;
+
+        int expected = a;
+        int actual = servise.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
